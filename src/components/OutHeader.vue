431f3388a7fb1bdea0d075ae91b1b7095b145732
0x7A7A6572 <2623385294@qq.com>
2021-08-14 22:37:12 +0800

    OutHeader模块

---
+++ src/components/OutHeader.vue
@@ -0,0 +1,14 @@
+<template>
+   <header id="continer">
+     <div id="left"></div>
+     <div id="right"></div>
+   </header>
+</template>
+<style scoped>
+#continer {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    background: #f4f4f4;
+}
+</style>
